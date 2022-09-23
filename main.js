c667b23212732aefc80cb2e0be517ebfeb945514
oneminch <27751688+oneminch@users.noreply.github.com>
2022-09-23 02:16:26 -0500

    bunch of stuff: add opensearch.xml template, add search and url sync capability, finish basic UI

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import Fuse from "fuse.js";
 import "virtual:windi.css";
-// import "https://fonts.bunny.net/css?family=fraunces:500,600";
 
 // todo update threshold
 // todo make efficient
+// todo sync query param with input
+// todo empty search results
+// todo responsive design
+// todo pre-search filtering available
+// todo init - loading fonts message
+// todo add opensearch spec
+// todo handle fetch exception
+// todo change search item UI
+// todo fix input delay from url query
+// todo rerun search on page change (back or forward)
+// todo refactor
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
@@ -14,17 +24,6 @@ let fontList = [],
 	fonts = "",
 	searchResults = [],
 	searchQuery = "";
-
-fetch("https://api.fontsource.org/v1/fonts")
-	.then((res) => {
-		if (res.ok) return res.json();
-		throw new Error("Network response was not ok.");
-	})
-	.then((data) => {
-		fontList = data.slice();
-		input.removeAttribute("disabled");
-		button.removeAttribute("disabled");
-	});
 
 class Query {
 	constructor(name, value) {
@@ -43,8 +42,8 @@ const createSearchQuery = (query) => {
 	return queryList;
 };
 
-const searchFonts = () => {
-	searchQuery = input.value.trim();
+const searchFonts = (query) => {
+	searchQuery = query.trim();
 
 	if (!searchQuery) {
 		return;
@@ -68,36 +67,80 @@ const populateResults = (results) => {
 
 	results.forEach((res) => {
 		fonts += `
-      
-				<details class="p-4 border-2 border-blue-gray-700 mb-4 rounded-md">
-					<summary class="flex justify-between items-center">
-						${res.family}
-						<a class="" href="https://fontsource.org/fonts/${res.id}" target="_blank">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  							<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-							</svg>
-						</a>
-					</summary>
-					<ul>
-						<li>Category: ${res.category}</li>
-						<li>Variable: ${res.variable}</li>
-						<li>Weights: ${res.weights.join(", ")}</li>
-						<li>Styles: ${res.styles.join(", ")}</li>
-						<li>Version: ${res.version}</li>
-					</ul>
-					<code>yarn add @fontsource/${res.id}</code>
-					<code>import "@fontsource/${res.id}"</code>
-					<code>body { font-family: "${res.family}", ${res.category}; }</code>
-				</details>
-      
+		<details
+		class="px-4 py-3 bg-blue-gray-800 mb-4 rounded-md focus-within:ring-2 ring-emerald-500 ring-opacity-50 ring-offset-0 rounded-md focus-within:outline-none">
+		<summary
+					class="flex w-full justify-between items-center cursor-pointer focus:outline-none">
+					<i class="gg-chevron-right-o"></i>
+					<span class="mr-auto">${res.family}</span>
+					<a
+						class="focus:outline-none focus:bg-blue-gray-900 rounded-full p-2 text-blue-gray-500"
+						href="https://fontsource.org/fonts/${res.id}"
+						target="_blank" rel="noopener">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							class="w-5 h-5">
+							<path
+							stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+						</svg>
+					</a>
+				</summary>
+				<ul class="mt-3">
+					<li>Category: ${res.category}</li>
+					<li>Variable: ${res.variable}</li>
+					<li>Weights: ${res.weights.join(", ")}</li>
+					<li>Styles: ${res.styles.join(", ")}</li>
+					<li>Version: ${res.version}</li>
+				</ul>
+				<code>yarn add @fontsource/${res.id}</code>
+				<code>import "@fontsource/${res.id}"</code>
+				<code>body { font-family: "${res.family}", ${res.category}; }</code>
+			</details>
     `;
 
 		output.innerHTML = fonts;
 	});
 };
 
+let parsedUrl = new URL(window.location.href);
+let urlQuery = parsedUrl.searchParams.get("q");
+
+fetch("https://api.fontsource.org/v1/fonts")
+	.then((res) => {
+		if (res.ok) return res.json();
+		throw new Error("Network response was not ok.");
+	})
+	.then((data) => {
+		fontList = data.slice();
+		input.removeAttribute("disabled");
+		button.removeAttribute("disabled");
+
+		if (urlQuery) {
+			input.value = urlQuery;
+			searchFonts(urlQuery);
+			populateResults(searchResults);
+		}
+	})
+	.catch((err) => {
+		console.log("An error has occured.");
+	});
+
+input.addEventListener("keypress", (e) => {});
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	searchFonts();
+
+	// update url parameters with new search query
+	parsedUrl = new URLSearchParams(window.location.search);
+	parsedUrl.set("q", input.value);
+	window.history.pushState({}, "", `?${parsedUrl}`);
+
+	searchFonts(input.value);
 	populateResults(searchResults);
 });
